7032cc48b40e11d4963f610ca243c8a8a1a0ce82
Gerald Hoheneder <gerald.hoheneder@gmail.com>
2014-07-30 23:30:02 +0200

    Redirect on save

--- src/main/java/com/comunio/controller/ApplicationController.java
+++ src/main/java/com/comunio/controller/ApplicationController.java
@@ -51,75 +51,77 @@ public class ApplicationController {
 
     @RequestMapping(value = { "/index", "", "/" })
     public String setupForm(Map<String, Object> map) {
-        return "index";
+	return "index";
     }
 
     @RequestMapping("/register")
     public String addComunio() {
-        return "register";
+	return "register";
     }
 
     @RequestMapping(value = "/show/{comunioId}")
-    public String showComunio(@PathVariable Long comunioId, Map<String, Object> map) throws JsonGenerationException,
-            JsonMappingException, IOException {
-        sessionData.setComunio(comunioService.retrieveComunio(comunioId));
-        map.put("comunioJSON", objectMapper.writeValueAsString(sessionData.getComunio()));
-        return "overview";
+    public String showComunio(@PathVariable Long comunioId,
+	    Map<String, Object> map) throws JsonGenerationException,
+	    JsonMappingException, IOException {
+	sessionData.setComunio(comunioService.retrieveComunio(comunioId));
+	map.put("comunioJSON",
+		objectMapper.writeValueAsString(sessionData.getComunio()));
+	return "overview";
     }
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String addTeams(@RequestParam("teams") String teamsString, @RequestParam("comName") String comunioName,
-            @RequestParam("password") String password, @RequestParam("numberOfTeams") String numberOfTeams,
-            Map<String, Object> map) {
+    public String addTeams(@RequestParam("teams") String teamsString,
+	    @RequestParam("comName") String comunioName,
+	    @RequestParam("password") String password,
+	    @RequestParam("numberOfTeams") String numberOfTeams,
+	    Map<String, Object> map) throws JsonGenerationException,
+	    JsonMappingException, IOException {
 
-        Comunio comunio = comunioService.createComunio(comunioName, password);
-        sessionData.setComunio(comunio);
-        groupService.initializeGroups(Integer.parseInt(numberOfTeams), teamsString);
-        sessionData.setComunio(comunioService.retrieveComunio(comunio.getComunioId()));
-        List<Groupe> groups = new ArrayList<Groupe>(groupService.getGroups());
-        map.put("comunio", sessionData.getComunio());
-        Groupe group = groupService.getGroup("A");
-        map.put("group", group);
-        map.put("teams", group.getSortedTeams());
-        map.put("groupNames", groupService.determineGroupNames(groups.size()));
-
-        map.put("matchdays", matchdayService.getSortedMatchdays(group.getFixture()));
-        return "overview";
+	Comunio comunio = comunioService.createComunio(comunioName, password);
+	sessionData.setComunio(comunio);
+	groupService.initializeGroups(Integer.parseInt(numberOfTeams),
+		teamsString);
+	sessionData.setComunio(comunioService.retrieveComunio(comunio
+		.getComunioId()));
+	map.put("comunioJSON",
+		objectMapper.writeValueAsString(sessionData.getComunio()));
+	return "overview";
     }
 
     @RequestMapping("/admin")
     public String admin(Map<String, Object> map) {
-        List<Team> teams = comunioService.getAllTeams();
-        try {
-            map.put("teams", objectMapper.writeValueAsString(teams));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        map.put("numberOfMatchdays", NUMBER_OF_MATCHDAYS);
-        map.put("comunio", sessionData.getComunio());
-        return "admin";
+	List<Team> teams = comunioService.getAllTeams();
+	try {
+	    map.put("teams", objectMapper.writeValueAsString(teams));
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+	map.put("numberOfMatchdays", NUMBER_OF_MATCHDAYS);
+	map.put("comunio", sessionData.getComunio());
+	return "admin";
     }
 
     @RequestMapping("/updateComunio/{comunioId}")
-    public void updateComunio(@RequestBody String jsonString, @PathVariable String comunioId) {
-        List<?> resultList = parseResultListFromJSON(jsonString);
-        resultService.updateResult(resultList, Long.parseLong(comunioId));
+    public void updateComunio(@RequestBody String jsonString,
+	    @PathVariable String comunioId) {
+	List<?> resultList = parseResultListFromJSON(jsonString);
+	resultService.updateResult(resultList, Long.parseLong(comunioId));
     }
 
     @RequestMapping("/showPlayoff")
     public String showPlayoff(Map<String, Object> map) {
-        map.put("comunio", sessionData.getComunio());
-        return "playoff";
+	map.put("comunio", sessionData.getComunio());
+	return "playoff";
 
     }
 
     private List<?> parseResultListFromJSON(String jsonString) {
-        List<?> jsonObject = null;
-        try {
-            jsonObject = objectMapper.readValue(jsonString, ArrayList.class);
-        } catch (final Exception e) {
-            e.printStackTrace();
-        }
-        return jsonObject;
+	List<?> jsonObject = null;
+	try {
+	    jsonObject = objectMapper.readValue(jsonString, ArrayList.class);
+	} catch (final Exception e) {
+	    e.printStackTrace();
+	}
+	return jsonObject;
     }
 }
--- src/main/java/com/comunio/service/impl/RoundRobinServiceImpl.java
+++ src/main/java/com/comunio/service/impl/RoundRobinServiceImpl.java
@@ -2,9 +2,11 @@ package com.comunio.service.impl;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 import com.comunio.model.Fixture;
 import com.comunio.model.Game;
@@ -16,106 +18,110 @@ public class RoundRobinServiceImpl {
     private static final String DUMMY_TEAM = "SWCW13Q56";
 
     public Fixture roundRobinCreateFixture(List<Team> teams) {
-        Fixture fixture = new Fixture();
-        fixture.setMatchdays(createMatchdays(teams, fixture));
-        return fixture;
+	Fixture fixture = new Fixture();
+	fixture.setMatchdays(createMatchdays(teams, fixture));
+	return fixture;
     }
 
-    private Set<Matchday> createMatchdays(List<Team> teamsOriginal, Fixture fixture) {
-        List<Team> teams = new ArrayList<>(teamsOriginal);
-        Set<Matchday> matchdays = new LinkedHashSet<>();
-        List<Team> team1 = new ArrayList<>();
-        List<Team> team2 = new ArrayList<>();
-        List<Team> team3 = new ArrayList<>();
-        List<Team> team4 = new ArrayList<>();
+    private Set<Matchday> createMatchdays(List<Team> teamsOriginal,
+	    Fixture fixture) {
+	List<Team> teams = new ArrayList<>(teamsOriginal);
+	Set<Matchday> matchdays = new HashSet<>();
+	List<Team> team1 = new ArrayList<>();
+	List<Team> team2 = new ArrayList<>();
+	List<Team> team3 = new ArrayList<>();
+	List<Team> team4 = new ArrayList<>();
 
-        int numberOfRounds = determineNumberOfRounds(teams);
+	int numberOfRounds = determineNumberOfRounds(teams);
 
-        if (teams.size() % 2 != 0) {
-            Team dummyTeam = new Team();
-            dummyTeam.setTeamName(DUMMY_TEAM);
-            teams.add(dummyTeam);
-        }
+	if (teams.size() % 2 != 0) {
+	    Team dummyTeam = new Team();
+	    dummyTeam.setTeamName(DUMMY_TEAM);
+	    teams.add(dummyTeam);
+	}
 
-        team1 = teams.subList(0, teams.size() / 2);
-        team2 = teams.subList(teams.size() / 2, teams.size());
+	team1 = teams.subList(0, teams.size() / 2);
+	team2 = teams.subList(teams.size() / 2, teams.size());
 
-        for (int i = 0; i < numberOfRounds; i++) {
-            for (int comunioMatchdayNumber = 1; comunioMatchdayNumber < teams.size(); comunioMatchdayNumber++) {
-                matchdays.add(createMatches(team1, team2, fixture, numberOfRounds));
-                Collections.rotate(team1, 1);
-                Collections.rotate(team2, -1);
-                team3 = new ArrayList<>(team1);
-                team4 = new ArrayList<>(team2);
+	for (int i = 0; i < numberOfRounds; i++) {
+	    for (int comunioMatchdayNumber = 1; comunioMatchdayNumber < teams
+		    .size(); comunioMatchdayNumber++) {
+		matchdays.add(createMatches(team1, team2, fixture,
+			numberOfRounds));
+		Collections.rotate(team1, 1);
+		Collections.rotate(team2, -1);
+		team3 = new ArrayList<>(team1);
+		team4 = new ArrayList<>(team2);
 
-                team4.set(team4.size() - 1, team1.get(0));
-                team3.set(0, team1.get(1));
-                team3.set(1, team2.get(team2.size() - 1));
+		team4.set(team4.size() - 1, team1.get(0));
+		team3.set(0, team1.get(1));
+		team3.set(1, team2.get(team2.size() - 1));
 
-                team1 = new ArrayList<>(team3);
-                team2 = new ArrayList<>(team4);
-            }
-        }
-        matchdays = new LinkedHashSet(setMatchdayNumbers(new ArrayList(matchdays)));
-        return matchdays;
+		team1 = new ArrayList<>(team3);
+		team2 = new ArrayList<>(team4);
+	    }
+	}
+	matchdays = new TreeSet<Matchday>(setMatchdayNumbers(new ArrayList<Matchday>(matchdays)));
+	return matchdays;
     }
 
-    private Matchday createMatches(List<Team> team1, List<Team> team2, Fixture fixture, int numberOfRounds) {
-        Matchday matchday = new Matchday();
-        matchday.setFixture(fixture);
-        List<Game> games = new ArrayList<>();
+    private Matchday createMatches(List<Team> team1, List<Team> team2,
+	    Fixture fixture, int numberOfRounds) {
+	Matchday matchday = new Matchday();
+	matchday.setFixture(fixture);
+	List<Game> games = new ArrayList<>();
 
-        for (int j = 0; j < team1.size(); j++) {
-            Game game = new Game();
-            game.setMatchday(matchday);
-            game.setHomeTeam(team1.get(j));
-            game.setAwayTeam(team2.get(j));
-            if (gameHasDummyTeam(game)) {
-                if (homeTeamIsDummy(game)) {
-                    matchday.setByeTeam(game.getAwayTeam());
-                }
-                if (awayTeamIsDummy(game)) {
-                    matchday.setByeTeam(game.getHomeTeam());
-                }
-            } else {
-                games.add(game);
-            }
-        }
+	for (int j = 0; j < team1.size(); j++) {
+	    Game game = new Game();
+	    game.setMatchday(matchday);
+	    game.setHomeTeam(team1.get(j));
+	    game.setAwayTeam(team2.get(j));
+	    if (gameHasDummyTeam(game)) {
+		if (homeTeamIsDummy(game)) {
+		    matchday.setByeTeam(game.getAwayTeam());
+		}
+		if (awayTeamIsDummy(game)) {
+		    matchday.setByeTeam(game.getHomeTeam());
+		}
+	    } else {
+		games.add(game);
+	    }
+	}
 
-        matchday.setMatches(new LinkedHashSet<>(games));
-        return matchday;
+	matchday.setMatches(new LinkedHashSet<>(games));
+	return matchday;
     }
 
     private List<Matchday> setMatchdayNumbers(List<Matchday> matchdays) {
-        int i = 1;
-        for (Matchday matchday : matchdays) {
-            matchday.setComunioMatchdayNumber(i);
-            i++;
-        }
-        return matchdays;
+	int i = 1;
+	for (Matchday matchday : matchdays) {
+	    matchday.setComunioMatchdayNumber(i);
+	    i++;
+	}
+	return matchdays;
     }
 
     private boolean awayTeamIsDummy(Game game) {
-        return game.getAwayTeam().getTeamName().equals(DUMMY_TEAM);
+	return game.getAwayTeam().getTeamName().equals(DUMMY_TEAM);
     }
 
     private boolean homeTeamIsDummy(Game game) {
-        return game.getHomeTeam().getTeamName().equals(DUMMY_TEAM);
+	return game.getHomeTeam().getTeamName().equals(DUMMY_TEAM);
     }
 
     private boolean gameHasDummyTeam(Game game) {
-        return awayTeamIsDummy(game) || homeTeamIsDummy(game);
+	return awayTeamIsDummy(game) || homeTeamIsDummy(game);
     }
 
     private int determineNumberOfRounds(List<Team> teams) {
-        switch (teams.size()) {
-        case 4:
-            return 3;
-        case 5:
-            return 2;
-        case 6:
-            return 2;
-        }
-        return 1;
+	switch (teams.size()) {
+	case 4:
+	    return 3;
+	case 5:
+	    return 2;
+	case 6:
+	    return 2;
+	}
+	return 1;
     }
 }
